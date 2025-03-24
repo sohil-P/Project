242d842cfb949642c5ec50d757d978233ce88b6f
Hiten-Prajapati585 <hitenprajapati6659@gmail.com>
2025-03-24 13:32:48 +0530

    update file

--- forgot_paasowrd.php
+++ forgot_paasowrd.php
@@ -1,5 +1,4 @@
-
- <?php
+<?php
 session_start();
 include_once 'config.php';
 
@@ -13,18 +12,59 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = $check_email->get_result();
 
     if ($result->num_rows > 0) {
-        $_SESSION['reset_email'] = $email;
-        header("Location: ./reset_password.php");
-        exit();
+        // Generate a unique token
+        $reset_token = bin2hex(random_bytes(16));
+        date_default_timezone_set('Asia/Kolkata');
+        $date = date('Y-m-d H:i:s', strtotime('+1 hour'));
+
+        // Store the token in the database
+        $query = "UPDATE `user_registration` SET `Reset_Token`='$reset_token', `Token_Expire`='$date' WHERE `Email`='$email'";
+        if (mysqli_query($conn, $query)) {
+            // Send the reset link to the user's email
+            $reset_link = "http://localhost/Project/reset_password.php?email=$email&reset_token=$reset_token";
+            $subject = "Password Reset Request";
+            $message = "Click the following link to reset your password: $reset_link";
+            $headers = "From: no-reply@gmail.com";
+
+            if (mail($email, $subject, $message,$headers)) {
+                echo "
+                <script>
+                alert('Reset link sent to your email');
+                window.location.href='./forgot_paasowrd.php';
+                </script>";
+            } else {
+                echo "
+                <script>
+                alert('Failed to send email. Please try again.');
+                window.location.href='forgot_paasowrd.php';
+                </script>";
+            }
+        } else {
+            echo "
+            <script>
+            alert('Server Down! Try again later');
+            window.location.href='forgot_paasowrd.php';
+            </script>";
+        }
     } else {
-        echo "<script>alert('Email not found!'); window.location.href='login.html';</script>";
+        echo "
+        <script>
+        alert('Email not found');
+        window.location.href='./forgot_paasowrd.php';
+        </script>";
     }
 }
 ?> 
 
-<form action="./forgot_paasowrd.php" method="post">
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
-    <input type="submit" value="Submit">
-</form>
+<div class="form-box" id="forgot-box">
+    <h2>Forgot Password</h2>
+    <form action="forgot_paasowrd.php" method="post">
+        <div class="input-box">
+            <input type="email" name="email" required placeholder="Enter your email">
+            <label>Email</label>
+        </div>
+        <button type="submit" class="btn">Send Reset Link</button>
+    </form>
+</div>
+
 
